27d9122d1a7042428e602eb9e7721d018042e353
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2023-11-23 22:22:32 -0400

    new changes in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,21 @@ import React, { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    // Si el usuario está en la raíz del sitio, redirigir a /docs/intro
-    if (window.location.pathname === '/') {
+    const path = window.location.pathname;
+
+    // Verifica si la URL ya contiene el código de idioma
+    if (path.startsWith('/es/')) {
+      // Si estamos en español pero no en la página de introducción, redirigir allí
+      if (path !== '/es/docs/intro') {
+        window.location.href = '/es/docs/intro';
+      }
+    } else if (path === '/') {
+      // Si estamos en la raíz (idioma inglés por defecto), redirigir a la página de introducción en inglés
       window.location.href = '/docs/intro';
     }
+    // No se necesita un else aquí, porque si ya estamos en /docs/intro (en inglés), no queremos hacer nada
   }, []);
 
-  // Mientras se realiza la redirección, puedes mostrar un contenido de carga o dejarlo en blanco
   return <div></div>;
 }
 
