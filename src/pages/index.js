3e5a0dc8fef1e6b8574278fab4ab9d3c96248ce9
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2023-11-23 22:35:22 -0400

    added new update to check

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,20 @@ import React, { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    const path = window.location.pathname;
+    const currentPath = window.location.pathname;
 
-    // Verifica si la URL ya contiene el código de idioma
-    if (path.startsWith('/es/')) {
-      // Si estamos en español pero no en la página de introducción, redirigir allí
-      if (path !== '/es/docs/intro') {
-        window.location.href = '/es/docs/intro';
-      }
-    } else if (path === '/') {
-      // Si estamos en la raíz (idioma inglés por defecto), redirigir a la página de introducción en inglés
-      window.location.href = '/docs/intro';
+    // Asumiendo que '/docs/intro' es la página de inicio para inglés
+    // y '/es/docs/intro' para español
+    const shouldBeEnglishHome = currentPath === '/' || currentPath === '/docs/intro';
+    const shouldBeSpanishHome = currentPath === '/es/' || currentPath === '/es/docs/intro';
+
+    // Si no estamos en una de las páginas de inicio, redirigir y recargar
+    if (!shouldBeEnglishHome && !shouldBeSpanishHome) {
+      const newPath = currentPath.startsWith('/es/') ? '/es/docs/intro' : '/docs/intro';
+      window.location.href = newPath;
+      window.location.reload();
     }
-    // No se necesita un else aquí, porque si ya estamos en /docs/intro (en inglés), no queremos hacer nada
   }, []);
 
   return <div></div>;
 }
-
